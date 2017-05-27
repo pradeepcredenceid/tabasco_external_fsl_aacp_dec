ed9869a1623cff599c6ea3d63fddb49b306153d8
Gary Bisson <gary.bisson@boundarydevices.com>
2017-05-27 13:55:50 +0200

    Import fsl_aacp_dec_N7.1.1 release
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- Android.mk
+++ Android.mk
@@ -3,6 +3,28 @@ ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+ifeq ($(BOARD_SOC_TYPE),IMX8DQ)
+
+LOCAL_MODULE := lib_aacplus_dec_v2_arm11_elinux
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := lib_aacplus_dec_v2_arm11_elinux.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lib_aacplusd_wrap_arm12_elinux_android
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT)/lib
+LOCAL_SRC_FILES_32 := lib_aacplusd_wrap_arm12_elinux_android.so
+include $(BUILD_PREBUILT)
+
+else
 LOCAL_PREBUILT_LIBS := \
 		lib_aacplus_dec_v2_arm11_elinux.so \
         lib_aacplusd_wrap_arm12_elinux_android.so
@@ -10,4 +32,5 @@ LOCAL_PREBUILT_LIBS := \
 LOCAL_MODULE_TAGS := eng
 include $(BUILD_MULTI_PREBUILT)
 
+endif
 endif
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 
-Method 1: Build into android rootfs image.
+Build into android rootfs image.
 
 Step 1: Install codec library.
 
@@ -9,22 +9,4 @@ Step 1: Install codec library.
 	source build/envsetup.sh
 	lunch // and select build name from menu
 	make
-
-
-Method 2: Install into existed android rootfs.
-
-Step 1: Install codec library.
-
-For NFS rootfs:
-
-	tar xzvf fsl_aacp_dec.tar.gz   
-	cd fsl_aacp_dec/
-	cp *.so <your_nfs_rootfs>/system/lib
-	 
-For SD rootfs:
-	  
-	tar xzvf fsl_aacp_dec.tar.gz
-	cd  fsl_aacp_dec/
-	mount -t ext3 -o loop system.img /mnt
-	cp *.so /mnt/lib
 
